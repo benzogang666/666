93a5afc373518060ac4417808404649652bc3311
benzogang666 <alikhankaiyrbay@gmail.com>
2024-10-25 01:59:31 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="side">
+        <div className="rows">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+        <div className="rows"></div>
+        <div className="rows"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
